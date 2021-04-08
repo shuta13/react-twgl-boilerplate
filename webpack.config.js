8ce6f0b27d3384b9f9ef5c63b73ec099f1073fdd
shuta13 <hollystarsun@gmail.com>
2021-04-08 16:40:47 +0900

    chore: build

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,46 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.tsx',
+  mode: "production",
+  entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist/"),
+    filename: "js/bundle.js",
   },
   module: {
     rules: [
       {
-        loader: 'ts-loader',
+        loader: "ts-loader",
         test: /\.tsx?$/,
         exclude: [/node_modules/],
         options: {
-          configFile: 'tsconfig.json',
+          configFile: "tsconfig.json",
         },
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
+    extensions: [".js", ".ts", ".tsx"],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public/'),
+    contentBase: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: 'http://localhost:3000/',
+    publicPath: "http://localhost:3000/",
     hotOnly: true,
+    historyApiFallback: true,
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      inject: false,
+      template: "./public/index.html",
     }),
   ],
   optimization: {
     minimizer: [
       (compiler) => {
-        const TerserPlugin = require('terser-webpack-plugin');
+        const TerserPlugin = require("terser-webpack-plugin");
         new TerserPlugin({
           extractComments: false,
         }).apply(compiler);
