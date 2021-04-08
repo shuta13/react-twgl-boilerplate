ceab3c87dee659c653e953b9130570cadd78cf6c
shuta13 <hollystarsun@gmail.com>
2021-04-08 22:09:16 +0900

    chore: use babel-loader instead of ts-loader

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = (api) => {
+  api.cache(true);
+
+  const presets = [
+    '@babel/preset-env',
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    '@babel/preset-typescript',
+  ];
+
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-syntax-dynamic-import',
+  ];
+
+  return { presets, plugins };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,56 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.tsx",
+  mode: isDevelopment ? 'development' : 'production',
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    filename: "js/bundle.js",
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'js/bundle.js',
   },
   module: {
     rules: [
       {
-        loader: "ts-loader",
         test: /\.tsx?$/,
-        exclude: [/node_modules/],
-        options: {
-          configFile: "tsconfig.json",
-        },
+        exclude: [/node_modules/, /dist/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                isDevelopment && require.resolve('react-refresh/babel'),
+              ].filter(Boolean),
+            },
+          },
+        ],
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".ts", ".tsx"],
+    extensions: ['.js', '.ts', '.tsx'],
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    contentBase: path.join(__dirname, 'public/'),
     port: 3000,
-    publicPath: "http://localhost:3000/",
+    publicPath: 'http://localhost:3000/',
     hotOnly: true,
     historyApiFallback: true,
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    isDevelopment && new webpack.HotModuleReplacementPlugin(),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
   optimization: {
     minimizer: [
       (compiler) => {
-        const TerserPlugin = require("terser-webpack-plugin");
+        const TerserPlugin = require('terser-webpack-plugin');
         new TerserPlugin({
           extractComments: false,
         }).apply(compiler);
